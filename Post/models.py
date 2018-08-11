982fd2445b3849532e2ff7481c992756724d522a
Baguvix <efanov.gag.48@gmail.com>
2018-08-11 15:06:44 +0300

    add superUser, register model in admin.py and modifided model Post, add first record in dateBase

--- Post/models.py
+++ Post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length = 30)
@@ -10,8 +11,12 @@ class Post(models.Model):
     header = models.CharField(max_length = 300)
     discription = models.TextField()
     dateCreation = models.DateTimeField(auto_now_add = True)
-    datePublic = models.DateTimeField(auto_now = True, editable = True)
+    datePublic = models.DateTimeField(blank = True)
     categorys = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.header
+
+    def save(self, *args, **kwargs):
+        self.datePublic = timezone.now()
+        return super(Post, self).save(*args, **kwargs)
