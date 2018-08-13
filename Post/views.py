7cc9b915afd5364c460f4b3e3d0910c227e3e0f1
Baguvix72 <efanov.gag.48@gmail.com>
2018-08-13 14:00:23 +0300

    add simple views and urlpathing

--- Post/views.py
+++ Post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Post.models import Post, Category
 
-# Create your views here.
+def listPage(request):
+    return HttpResponse("1\t2\n")
+    #return render(request, 'Post/list.html')
