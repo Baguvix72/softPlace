ea730ee5cbe67b1a4643bf4095eaeebbefda56ec
Baguvix72 <efanov.gag.48@gmail.com>
2018-08-15 14:15:23 +0300

    Work with templates, add new records in database, change views

--- Post/views.py
+++ Post/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Post.models import Post, Category
 
-def simple_listPage(request):
-    AllPost = Post.objects.all()
-    AllCategory = Category.objects.all()
-    resulSTR = ''
-    for postOb in AllPost:
-        resulSTR = resulSTR + postOb.header + ' '
-        categoryList = AllCategory.filter(post = postOb)
-        for categor in categoryList:
-            resulSTR = resulSTR + categor.name + ' '
-    return HttpResponse(resulSTR)
-
 def listPage(request):
     AllPost = Post.objects.all().order_by('-datePublic')
     AllCategory = Category.objects.all()
-    vars = {'AllPost': AllPost, 'AllCategory': AllCategory}
+
+    PostsFirsts_12 = AllPost[0:12]
+    PostCount = PostsFirsts_12.count()
+
+    FirstGrup = AllPost[0:3]
+    SecondGrup = AllPost[3:6]
+    ThriedGrup = AllPost[6:9]
+    FourGrup = AllPost[9:12]
+
+    ListPost = [FirstGrup, SecondGrup, ThriedGrup, FourGrup]
+
+    vars = {'Posts': PostsFirsts_12, 'AllCategory': AllCategory, 'ListPost':ListPost}
     return render(request, 'Post/list.html', vars)
+
+    #def GetRowCount(self, PostCount):
