9d74200f6c8f79de00fa4a0b34bc04b7f21590e5
Baguvix <efanov.gag.48@gmail.com>
2018-08-19 12:25:35 +0300

    Work with category list

--- Post/views.py
+++ Post/views.py
@@ -7,10 +7,14 @@ def listPage(request, numPage, category):
     AllPost = Post.objects.all().order_by('-datePublic')
     AllCategory = Category.objects.all()
 
+    if category != "all":
+        CurrentCategory = AllCategory.get(urlName = category)
+        AllPost = AllPost.filter(categorys = CurrentCategory)
+
     PageCount = GetIntCount(AllPost.count(), 12)
     
     ListPagin = GetListPagin(PageCount, numPage)
     ListPosts = GetPagePost(AllPost, numPage)
 
-    vars = {'ListPost':ListPosts, 'numPage':numPage, 'ListPagin':ListPagin, 'PageCount':PageCount, 'Category':AllCategory}
+    vars = {'ListPost':ListPosts, 'numPage':numPage, 'ListPagin':ListPagin, 'PageCount':PageCount, 'SetCategory':AllCategory, 'ActiveCategory':category}
     return render(request, 'Post/list.html', vars)
